395cba2921a8653ccd9263bba0fcccd483a8ca4d
chattta <135157990+chattta@users.noreply.github.com>
2023-06-20 23:42:10 +0800

    Update cn.ts

--- app/locales/cn.ts
+++ app/locales/cn.ts
@@ -9,7 +9,7 @@ const cn = {
   },
   Auth: {
     Title: "需要密码",
-    Tips: "管理员开启了密码验证，请在下方填入访问码",
+    Tips: "为防止被滥用，扫码添加好友，发送AI获取访问码免费使用",
     Input: "在此处填写访问码",
         Image: Gzh,
     Confirm: "确认",
